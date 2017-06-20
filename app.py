39acf8108c9e97164c183c5fbf0c0766532c2bf4
alishobeiri <alishobeiri@ymail.com>
2017-06-20 17:19:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import os
 
 from plotly import graph_objs as go
 from flask import Flask
