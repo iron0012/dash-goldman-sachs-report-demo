352ab883ebc2aea9aecc68e3e0fe58ccd880d666
alishobeiri <alishobeiri@ymail.com>
2017-06-20 23:31:49 -0400

    Changed report

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from plotly import graph_objs as go
 from datetime import datetime as dt
 import json
 import pandas as pd
-
+import os
 from flask import Flask
 server = Flask('my app')
 
@@ -60,13 +60,13 @@ modifed_perf_table = make_dash_table( df_perf_summary )
 
 # In[8]:
 
-modifed_perf_table.insert( 
-    0, html.Tr( [ 
-            html.Td([]), 
-            html.Td(['Cumulative'], colSpan=4, style=dict(textAlign="center")), 
-            html.Td(['Annualised'], colSpan=4, style=dict(textAlign="center")) 
+modifed_perf_table.insert(
+    0, html.Tr( [
+            html.Td([]),
+            html.Td(['Cumulative'], colSpan=4, style=dict(textAlign="center")),
+            html.Td(['Annualised'], colSpan=4, style=dict(textAlign="center"))
         ], style = dict( background='white', fontWeight=600 )
-    ) 
+    )
 )
 
 
@@ -85,29 +85,29 @@ dash.__version__
 
 # In[11]:
 
-app = dash.Dash('GS Bond II Portfolio', server=server)
+app = dash.Dash('GS Bond II Portfolio', server=server, url_base_pathname='/dash/gallery/goldman-sachs-report/', csrf_protect=False)
 
 # Describe the layout, or the UI, of the app
-app.layout = html.Div([        
-        
+app.layout = html.Div([
+
     html.Div([ # page 1
 
-        html.A([ 'Print PDF' ], 
-           className="button no-print", 
-           style=dict(position="absolute", top=-40, right=0)),     
-                        
+        html.A([ 'Print PDF' ],
+           className="button no-print",
+           style=dict(position="absolute", top=-40, right=0)),
+
         html.Div([ # subpage 1
 
             # Row 1 (Header)
 
             html.Div([
 
-                html.Div([      
+                html.Div([
                     html.H5('Goldman Sachs Strategic Absolute Return Bond II Portfolio'),
                     html.H6('A sub-fund of Goldman Sachs Funds, SICAV', style=dict(color='#7F90AC')),
                     ], className = "nine columns padded" ),
 
-                html.Div([            
+                html.Div([
                     html.H1([html.Span('03', style=dict(opacity=0.5)), html.Span('17')]),
                     html.H6('Monthly Fund Update')
                 ], className = "three columns gs-header gs-accent-header padded", style=dict(float='right') ),
@@ -118,7 +118,7 @@ app.layout = html.Div([
 
             # Row 2
 
-            html.Div([     
+            html.Div([
 
                 html.Div([
                     html.H6('Investor Profile', className = "gs-header gs-text-header padded"),
@@ -147,10 +147,10 @@ app.layout = html.Div([
                 ], className = "eight columns" ),
 
             ], className = "row "),
-                        
+
             # Row 2.5
 
-            html.Div([     
+            html.Div([
 
                 html.Div([
                     html.H6('Performance (%)', className = "gs-header gs-text-header padded"),
@@ -167,11 +167,11 @@ app.layout = html.Div([
                         can go down as well as up. A loss of capital may occur.")
                 ], className = "eight columns" ),
 
-            ], className = "row "),                        
+            ], className = "row "),
 
             # Row 3
 
-            html.Div([             
+            html.Div([
 
                 html.Div([
                     html.H6('Fund Data', className = "gs-header gs-text-header padded"),
@@ -184,40 +184,40 @@ app.layout = html.Div([
 
                     html.H6("Calendar Year Performance (%)", className = "gs-header gs-table-header padded"),
                     html.Table( make_dash_table( df_cal_year ) )
-                ], className = "eight columns" ),                      
+                ], className = "eight columns" ),
 
-            ], className = "row "),                
+            ], className = "row "),
 
         ], className = "subpage" ),
 
-    ], className = "page" ),  
-             
+    ], className = "page" ),
+
     html.Div([ # page 2
-                
-        html.A([ 'Print PDF' ], 
-           className="button no-print", 
-           style=dict(position="absolute", top=-40, right=0)),                
-                
+
+        html.A([ 'Print PDF' ],
+           className="button no-print",
+           style=dict(position="absolute", top=-40, right=0)),
+
         html.Div([ # subpage 2
-                        
+
             # Row 1 (Header)
-                        
+
             html.Div([
 
-                html.Div([      
+                html.Div([
                     html.H5('Goldman Sachs Strategic  Absolute Return Bond II Portfolio'),
                     html.H6('A sub-fund of Goldman Sachs Funds, SICAV', style=dict(color='#7F90AC')),
                     ], className = "nine columns padded" ),
 
-                html.Div([            
+                html.Div([
                     html.H1([html.Span('03', style=dict(opacity=0.5)), html.Span('17')]),
                     html.H6('Monthly Fund Update')
                 ], className = "three columns gs-header gs-accent-header padded", style=dict(float='right') ),
 
             ], className = "row gs-header gs-text-header"),
-                        
+
             # Row 2
-                        
+
             html.Div([
 
                 # Data tables on this page:
@@ -225,75 +225,76 @@ app.layout = html.Div([
                 # df_fund_info = pd.read_csv('https://plot.ly/~jackp/17544/.csv')
                 # df_fund_characteristics = pd.read_csv('https://plot.ly/~jackp/17542/.csv')
                 # df_fund_facts = pd.read_csv('https://plot.ly/~jackp/17540/.csv')
-                # df_bond_allocation = pd.read_csv('https://plot.ly/~jackp/17538/')                                
-                  
+                # df_bond_allocation = pd.read_csv('https://plot.ly/~jackp/17538/')
+
                 # Column 1
-                                
-                html.Div([      
+
+                html.Div([
                     html.H6('Financial Information', className = "gs-header gs-text-header padded"),
                     html.Table( make_dash_table( df_fund_info ) ),
-                                        
+
                     html.H6('Fund Characteristics', className = "gs-header gs-text-header padded"),
                     html.Table( make_dash_table( df_fund_characteristics ) ),
-                                        
+
                     html.H6('Fund Facts', className = "gs-header gs-text-header padded"),
                     html.Table( make_dash_table( df_fund_facts ) ),
-                                        
+
                 ], className = "four columns" ),
-                                
+
                 # Column 2
 
-                html.Div([                                          
+                html.Div([
                     html.H6('Sector Allocation (%)', className = "gs-header gs-table-header padded"),
                     html.Iframe(src="https://plot.ly/~jackp/17560.embed?modebar=false&link=false&autosize=true", \
                         seamless="seamless", style=dict(border=0), width="100%", height="300"),
-                                        
+
                     html.H6('Country Bond Allocation (%)', className = "gs-header gs-table-header padded"),
                     html.Table( make_dash_table( df_bond_allocation ) ),
-                                        
+
                 ], className = "four columns" ),
-                                
+
                 # Column 3
-                                
-                html.Div([            
+
+                html.Div([
                     html.H6('Top 10 Currency Weights (%)', className = "gs-header gs-table-header padded"),
                     html.Iframe(src="https://plot.ly/~jackp/17555.embed?modebar=false&link=false&autosize=true", \
                         seamless="seamless", style=dict(border=0), width="100%", height="300"),
-                                        
+
                     html.H6('Credit Allocation (%)', className = "gs-header gs-table-header padded"),
                     html.Iframe(src="https://plot.ly/~jackp/17557.embed?modebar=false&link=false&autosize=true", \
-                        seamless="seamless", style=dict(border=0), width="100%", height="300"),                                       
-                                                                                
-                ], className = "four columns" ),                                
+                        seamless="seamless", style=dict(border=0), width="100%", height="300"),
 
-            ], className = "row"),                        
-                        
+                ], className = "four columns" ),
+
+            ], className = "row"),
+
         ], className = "subpage" ),
-                
+
     ], className = "page" ),
-             
+
 ])
 
-
-# In[ ]:
+if 'DYNO' in os.environ:
+    app.scripts.append_script({
+        'external_url': 'https://cdn.rawgit.com/chriddyp/ca0d8f02a1659981a0ea7f013a378bbd/raw/e79f3f789517deec58f41251f7dbb6bee72c44ab/plotly_ga.js'
+    })
 
 external_css = [ "https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css",
         "https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
         "//fonts.googleapis.com/css?family=Raleway:400,300,600",
-        "https://codepen.io/plotly/pen/KmyPZr.css",
+        "https://cdn.rawgit.com/plotly/dash-app-stylesheets/5047eb29e4afe01b45b27b1d2f7deda2a942311a/goldman-sachs-report.css",
         "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"]
 
-for css in external_css: 
+for css in external_css:
     app.css.append_css({ "external_url": css })
-    
+
 external_js = [ "https://code.jquery.com/jquery-3.2.1.min.js",
-        "https://codepen.io/plotly/pen/KmyPZr.js" ]
-    
-for js in external_js: 
+        "https://cdn.rawgit.com/plotly/dash-app-stylesheets/a3401de132a6d0b652ba11548736b1d1e80aa10d/dash-goldman-sachs-report-js.js" ]
+
+for js in external_js:
     app.scripts.append_script({ "external_url": js })
 
 
 # In[ ]:
 if __name__ == '__main__':
 	app.server.run()
-
